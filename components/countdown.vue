dce92571f917183b820a3f44da8abfefdfe8336d
Nick Barry <itsnickbarry@protonmail.ch>
2020-10-29 16:00:18 -0600

    format countdown timer

--- components/countdown.vue
+++ components/countdown.vue
@@ -13,26 +13,25 @@
 </template>
 
 <script>
+let now = function () {
+  let remaining = 1604088000 - Math.floor(new Date().getTime() / 1000);
+  let hours = Math.floor(remaining / (60 * 60)).toString().padStart(2, '0');
+  let minutes = Math.floor(remaining % (60 * 60) / 60).toString().padStart(2, '0');
+  let seconds = Math.floor(remaining % 60).toString().padStart(2, '0');
+  return `${ hours }:${ minutes }:${ seconds }`;
+};
+
 export default {
   data: function () {
     return {
-      display: 0,
-      deadline: 1604088000,
+      display: now(),
     }
   },
 
   mounted: function () {
-    this.update();
     setInterval(function () {
-      this.update();
+      this.display = now();
     }.bind(this), 1000);
-  },
-
-  methods: {
-    update: function () {
-      let remaining = this.deadline - Math.floor(new Date().getTime() / 1000);
-      this.display = remaining;
-    },
   },
 }
 </script>
