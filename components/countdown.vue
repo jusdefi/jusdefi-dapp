21fcf615f4d9074572768841da1112fffba44e1c
Nick Barry <itsnickbarry@protonmail.ch>
2020-10-29 15:47:04 -0600

    add countdown timer

---
+++ components/countdown.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hero is-fullheight is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <div class="box">
+          <div class="display container has-text-centered">
+            {{ display }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      display: 0,
+      deadline: 1604088000,
+    }
+  },
+
+  mounted: function () {
+    this.update();
+    setInterval(function () {
+      this.update();
+    }.bind(this), 1000);
+  },
+
+  methods: {
+    update: function () {
+      let remaining = this.deadline - Math.floor(new Date().getTime() / 1000);
+      this.display = remaining;
+    },
+  },
+}
+</script>
+
+<style>
+  .display {
+    font-size: 10em !important;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <LiquidityEvent />
+  <Countdown />
 </template>
 
 <script>
+import Countdown from '../components/countdown.vue';
 import LiquidityEvent from '../components/liquidity_event.vue';
 
 export default {
-  components: { LiquidityEvent },
+  components: { Countdown, LiquidityEvent },
 }
 </script>
